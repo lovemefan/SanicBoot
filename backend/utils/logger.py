9932eac322aaed0a0556a348809f13ef98d62ef6
lovemefan <450489712@qq.com>
2021-03-04 16:59:49 +0800

    fix the some bugs

--- backend/utils/logger.py
+++ backend/utils/logger.py
@@ -5,19 +5,5 @@
 # @File    : logger.py
 import logging
 
-from backend.config.Config import Config
-
-
-def get(key, default=None):
-    """get value from config by the key file dynamically
-    Args:
-        key (str): the key of config file, example `host.ip`.
-        default (str): if the key is not exist,return default as value.
-    Returns:
-        str : the value of config file key example : if the key is `host.ip`
-        maybe it will return `localhost`
-    """
-    return Config.get_instance().get(key, default)
-
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]  - %(levelname)s - %(threadName)s - %(module)s.%(funcName)s - %(message)s')
 logger = logging.getLogger(__name__)
