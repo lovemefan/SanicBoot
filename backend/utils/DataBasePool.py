4d97c9673a10877cd1bc7de955ef4317ef825567
lovemefan <lovemefan@outlook.com>
2023-02-24 14:41:46 +0800

    add code comments

--- backend/utils/DataBasePool.py
+++ backend/utils/DataBasePool.py
@@ -7,11 +7,14 @@
 import asyncio
 import threading
 import time
+from typing import Union
+
 import aiomysql
 import pymysql
 from backend.config.BaseConfig import BaseConfig
 from backend.exception.SqlException import SQLException
 from backend.utils.logger import logger
+
 
 lock = threading.Lock()
 
@@ -38,26 +41,62 @@ class DataBasePool(BaseConfig):
             loop=asyncio.get_event_loop(),
             # max number usage of one connection,0 or None is no limits,default is 0
             maxsize=int(self.get('mysql.maxusage')),
+            autocommit=True,
             **config
         )
         self.__pool_db_init__ = False
 
-    async def execute(self, sql: str, query: bool = None, many: bool = False, data: list = None):
-        """execute sql by a connection from database pool
-        Args:
-            sql (str): sql
-            query (bool): is the type of sql is query or else(update insert)
-            many (bool): if the many is true, data will be available
-            data (list): when many if true, data will be loaded to sql execute
-        Returns:
-            list: results of sql or None
+    async def execute(self, sql: Union[str, list], query: bool = None, many: bool = False, data: list = None):
+        """Execute batch of sql statement method will execute the operation iterating over two ways:
 
-        Example:
+         1. update or insert in same table. insert or replace statements are optimized by batching the data,
+            that is using the MySQL multiple rows syntax.
+            sql cloud be str or list, if sql type is list, executemany is not available
 
+                Example: Inserting 3 new employees and their phone number
+
+                    data = [
+                        ('Jane','555-001'),
+                        ('Joe', '555-001'),
+                        ('John', '555-003')
+                        ]
+
+                    @Mysql.auto_execute_sql
+                    def insert_employee(many=True, data)
+                        stmt = "INSERT INTO employees (name, phone) VALUES ('%s','%s')"
+
+                    await insert_employee(data)
+
+
+
+        2. execute many sql statement in same transaction
+            when sql is list, you should specify the query,
+            otherwise it will set query type by Determine the first sql statement whether there is a substring of 'select'
+
+                Example: Inserting and update new employees
+
+
+                    @Mysql.auto_execute_sql
+                    def insert_employee(name, query=False)
+                        stmt = "INSERT INTO employees (name, phone) VALUES ('%s','%s')"
+                        stmt2 = f"UPDATE employees set name={name}"
+                        return [stmt, stmt2]
+
+                    await insert_employee(data)
         """
         # execute sql
-        sql = sql.strip().replace('\n', ' ').replace('  ', ' ')
-        logger.debug(f"Executing {sql}")
+        if isinstance(sql, str):
+            sql = [sql.strip().replace('\n', ' ').replace('  ', ' ')]
+            logger.debug(f"Executing {sql}")
+        elif isinstance(sql, list):
+            sql = [s.strip().replace('\n', ' ').replace('  ', ' ') for s in sql]
+            logger.debug(f"Executing many sql in same transaction")
+
+        if query is None:
+            if 'select' in sql[0].lower()[:6]:
+                query = True
+            else:
+                query = False
 
         result = ()
         # if query is None, auto set query is True if select in sql
@@ -67,13 +106,9 @@ class DataBasePool(BaseConfig):
             self.__pool_db_init__ = True
 
         async with self.pool_db.acquire() as conn:
+            # set connection into autocommit mode otherwise the 'select' will use wrong cache
             await conn.autocommit(True)
             async with conn.cursor(aiomysql.Cursor) as cursor:
-                if query is None:
-                    if 'select' in sql.lower()[:6]:
-                        query = True
-                    else:
-                        query = False
                 try:
                     start_time = time.time()
                     if not query:
@@ -83,9 +118,15 @@ class DataBasePool(BaseConfig):
 
                     # if set many true and update/insert sql and data is list not None, insert all
                     if many and not query and data is not None:
-                        await cursor.executemany(sql, data)
+                        if len(sql) != 1:
+                            raise ValueError("Using executemany must set sql as str instead of list "
+                                             "when set the many true and data not none ")
+                        await cursor.executemany(sql[0], data)
+                        logger.debug(f"Executing many: {sql[0]} of {data}")
                     else:
-                        await cursor.execute(sql)
+                        for s in sql:
+                            await cursor.execute(s)
+                            logger.debug(f"Executing: {s}")
                     result = await cursor.fetchall()
 
                     if not query:
