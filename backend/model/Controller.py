f4a8c6011524748c1e40212013afdf65fc4a485b
lovemefan <lovemefan@outlook.com>
2023-04-21 02:34:00 +0800

    extend HTTPMethodView

--- backend/model/Controller.py
+++ backend/model/Controller.py
@@ -3,7 +3,9 @@
 # @Time      :2023/3/30 00:56
 # @Author    :lovemefan
 # @Email     :lovemefan@outlook.com
+from sanic.views import HTTPMethodView
 
 
-class ControllerBase(object):
-    pass
+class ControllerBase(HTTPMethodView):
+    def __init__(self):
+        super(ControllerBase, self).__init__()
