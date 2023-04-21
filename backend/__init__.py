8417adc588ea297dfb22a3d8c6042e5bc619bf99
lovemefan <lovemefan@outlook.com>
2023-04-21 11:48:21 +0800

    import scan from config

--- backend/__init__.py
+++ backend/__init__.py
@@ -8,29 +8,71 @@ import importlib
 import os
 from pathlib import Path
 
+from backend.config.Config import Config
 from backend.utils.logger import logger
 
 project_root_path = os.path.dirname(__file__)
 
 # automatically import any Python files in the models/ directory
-module_dir = Path(os.path.dirname(__file__))
+module_dir = Path(os.path.dirname(os.path.dirname(__file__)))
 
 
 def import_models(module_dir, namespace):
     for file in Path(module_dir).rglob("*.py"):
         if not file.name.startswith("_"):
             file_path = str(file.absolute())
-            datasource_name = file_path[
+            module_name = file_path[
                 file_path.find(namespace.replace(".", "/")) : file_path.find(".py")
             ]
-            datasource_name = (
-                datasource_name.replace("/", ".").replace(namespace, "").strip(".")
+            module_name = (
+                module_name.replace("/", ".").replace(namespace, "").strip(".")
             )
-            logger.debug("Importing datasource: {}".format(datasource_name))
-            importlib.import_module(namespace + "." + datasource_name)
+            logger.debug("Importing module: {}".format(module_name))
+            importlib.import_module(namespace + "." + module_name)
 
 
-import_models(module_dir / "core/datasource", "backend.core.datasource")
-import_models(module_dir / "repository", "backend.repository")
-import_models(module_dir / "services", "backend.services")
-import_models(module_dir / "controllers", "backend.controllers")
+module_path = module_dir / Config.get_instance().get(
+    "server.component.datasource"
+).replace(".", "/").strip("/")
+module_namespace = Config.get_instance().get("server.component.datasource")
+if os.path.exists(module_path):
+    import_models(module_path, module_namespace)
+else:
+    raise ValueError(
+        f"{module_path} not exists, please check config of server.component.datasource"
+    )
+
+
+module_path = module_dir / Config.get_instance().get(
+    "server.component.repository"
+).replace(".", "/").strip("/")
+module_namespace = Config.get_instance().get("server.component.repository")
+if os.path.exists(module_path):
+    import_models(module_path, module_namespace)
+else:
+    raise ValueError(
+        f"{module_path} not exists, please check config of server.component.repository"
+    )
+
+module_path = module_dir / Config.get_instance().get(
+    "server.component.service"
+).replace(".", "/").strip("/")
+module_namespace = Config.get_instance().get("server.component.service")
+if os.path.exists(module_path):
+    import_models(module_path, module_namespace)
+else:
+    raise ValueError(
+        f"{module_path} not exists, please check config of server.component.service"
+    )
+
+
+module_path = module_dir / Config.get_instance().get(
+    "server.component.controller"
+).replace(".", "/").strip("/")
+module_namespace = Config.get_instance().get("server.component.controller")
+if os.path.exists(module_path):
+    import_models(module_path, module_namespace)
+else:
+    raise ValueError(
+        f"{module_path} not exists, please check config of server.component.controller"
+    )
