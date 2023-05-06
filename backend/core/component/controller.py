e4c9ee411c5456a21db49923461b2300bc208604
lovemefan <lovemefan@outlook.com>
2023-05-06 23:53:16 +0800

    support custom Uri

--- backend/core/component/controller.py
+++ backend/core/component/controller.py
@@ -3,6 +3,9 @@
 # @Time      :2023/3/30 00:26
 # @Author    :lovemefan
 # @Email     :lovemefan@outlook.com
+import inspect
+from typing import Union
+
 from sanic import Sanic
 from sanic_openapi import swagger_blueprint
 
@@ -16,35 +19,55 @@ app = Sanic("sanic-backend")
 app.blueprint(swagger_blueprint)
 
 
-def Controller(cls):
+def Controller(cls: Union[str, object]):
     """
     New controller can be added to sanic controller with the :func:`register_controller`
     function decorator.
     For example::
-        @register_controller()
+        @Controller
         class UserController(controllerBase):
             (...)
     .. note:: All controller must implement the :class:`controllerBase` interface.
     """
 
-    name = name_convert_to_snake(cls.__name__)
-    namespace = Config.get_instance().get("server.component.controller")
-    uri = f"{cls.__module__}.{cls.__name__}".replace(namespace, "").replace(".", "/")
-    uri = "/".join([name_convert_to_snake(i) for i in uri.split("/") if i])
+    def _registry_controller(name, uri, cls):
+        if name in CONTROLLERS_REGISTRY:
+            return CONTROLLERS_REGISTRY[name]
 
-    if name in CONTROLLERS_REGISTRY:
-        return CONTROLLERS_REGISTRY[name]
+        if isinstance(cls, object):
+            if not issubclass(cls, ControllerBase):
+                raise ValueError(
+                    "Model ({}: {}) must extend controllerBase".format(
+                        name, cls.__name__
+                    )
+                )
 
-    if not issubclass(cls, ControllerBase):
-        raise ValueError(
-            "Model ({}: {}) must extend controllerBase".format(name, cls.__name__)
+        CONTROLLERS_REGISTRY[name] = cls
+        logger.debug(f"Register controller {name} to {uri}")
+        app.add_route(cls.as_view(), uri)
+
+        return cls
+
+    if inspect.isclass(cls):
+        name = name_convert_to_snake(cls.__name__)
+        namespace = Config.get_instance().get("server.component.controller")
+        uri = f"{cls.__module__}.{cls.__name__}".replace(namespace, "").replace(
+            ".", "/"
         )
+        uri = "/".join([name_convert_to_snake(i) for i in uri.split("/") if i])
+        _registry_controller(name, uri, cls)
+    else:
 
-    CONTROLLERS_REGISTRY[name] = cls
-    logger.debug(f"Register controller {name} to {uri}")
-    app.add_route(cls.as_view(), uri)
+        def decorator(_cls):
+            name = name_convert_to_snake(_cls.__name__)
+            _registry_controller(name, cls, _cls)
 
-    return cls
+            def wrapper():
+                pass
+
+            return wrapper
+
+        return decorator
 
 
 def get_controller(name):
