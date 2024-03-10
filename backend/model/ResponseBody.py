87801d57f18fc6a421d08b50535237ee1e778409
lovemefan <lovemefan@outlook.com>
2024-03-10 15:31:27 +0800

    add response

--- backend/model/ResponseBody.py
+++ backend/model/ResponseBody.py
@@ -3,10 +3,21 @@
 # @Time    : 2020/12/25 下午9:49
 # @Author  : lovemefan
 # @File    : ResponseBody.py
+from sanic import json
+
+
 class ResponseBody:
     """The response body of http"""
 
-    def __init__(self, message="", data=None, code=None):
+    def __init__(self, message="", data=None, code=None, status: int = 200):
         self.message = message
         self.data = data
         self.code = code
+        self.status = status
+
+    @property
+    def response(self):
+        return json(
+            {"message": self.message, "data": self.data, "code": self},
+            status=self.status,
+        )
